ffd9976b805434753fe7d737dc408108f44437e8
sasa2754 <sasamor5@gmail.com>
2024-09-01 13:26:43 -0300

    Coracao

--- public/script.js
+++ public/script.js
@@ -4,11 +4,28 @@ const body = document.querySelector(`body`);
 
 let cont = 0;
 
+const moveHearts = async () => {
+    const heart = body.querySelectorAll(".heart");
+    
+    heart.forEach(x => x.classList.add('move'))
+}
+
 botao.addEventListener(`click`, () => {
     cont += 1;
     contador.textContent = `Contador: ${cont}`;
 
-    // if (cont > 10) {
-    //     body.style.transform = `rotate(2turn)`;
-    // }
+    if (cont % 15 == 0) {
+        const heart = body.querySelectorAll(".heart");
+
+        heart.forEach(x => x.remove())
+
+        body.insertAdjacentHTML("beforeend", `<img class="heart topleft" src="/heart.svg"/>`)
+        body.insertAdjacentHTML("beforeend", `<img class="heart bottomleft" src="/heart.svg"/>`)
+        body.insertAdjacentHTML("beforeend", `<img class="heart topright" src="/heart.svg"/>`)
+        body.insertAdjacentHTML("beforeend", `<img class="heart bottomright" src="/heart.svg"/>`)
+
+        setTimeout(moveHearts, 200);
+    }
 });
+
+
